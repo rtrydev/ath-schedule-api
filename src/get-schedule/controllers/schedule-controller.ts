4916cbcc1c56fbc48b882305e6497366a2106d05
rtrydev <r.kulka44@gmail.com>
2023-03-25 13:20:27 +0100

    Added handling for week variable used in ics fetch

--- src/get-schedule/controllers/schedule-controller.ts
+++ src/get-schedule/controllers/schedule-controller.ts
@@ -14,16 +14,35 @@ export class ScheduleController {
     private scheduleCacheS3: ScheduleCacheS3;
 
     async getSchedule(params: ScheduleFetchParams) {
+        const weekOffset = 2172;
         const cachedSchedule = await this.scheduleCacheS3.getCachedSchedule(params.id);
         let schedule;
+
+        const currentDate = new Date();
+
+        const first = currentDate.getDate() - currentDate.getDay() + 1;
+        const firstDay = new Date(currentDate.setDate(first));
+        const firstDayTimeReset = new Date(firstDay.setHours(3));
+
+        const weekStartTimestamp = Math.floor(firstDayTimeReset.getTime() / 1000);
+        const weekEndTimestamp = weekStartTimestamp + 60 * 60 * 24 * 7;
+
+        const fetchParams = {
+            id: params.id,
+            type: params.type,
+            fromDate: params.fromDate || weekStartTimestamp,
+            toDate: params.toDate || weekEndTimestamp
+        }
 
         if (cachedSchedule) {
             console.log('Using cached schedule');
             schedule = this.scheduleParseService.parseIcsToJson(cachedSchedule);
         } else {
             console.log('Fetching new schedule');
+            const week = Math.floor(params.fromDate / 60 / 60 / 24 / 7) - weekOffset;
+
             const fetchedSchedule = await this.scheduleFetchService
-                .fetchIcsFile(params.id, params.type);
+                .fetchIcsFile(params.id, params.type, week);
 
             schedule = this.scheduleParseService.parseIcsToJson(fetchedSchedule);
 
@@ -32,12 +51,12 @@ export class ScheduleController {
             }
         }
 
-        if (params.fromDate) {
-            schedule = schedule.filter(scheduleItem => scheduleItem.startTime > params.fromDate);
+        if (fetchParams.fromDate) {
+            schedule = schedule.filter(scheduleItem => scheduleItem.startTime > fetchParams.fromDate);
         }
 
-        if (params.toDate) {
-            schedule = schedule.filter(scheduleItem => scheduleItem.endTime < params.toDate);
+        if (fetchParams.toDate) {
+            schedule = schedule.filter(scheduleItem => scheduleItem.endTime < fetchParams.toDate);
         }
 
         return schedule;
--- src/get-schedule/services/schedule-fetch-service.ts
+++ src/get-schedule/services/schedule-fetch-service.ts
@@ -4,8 +4,8 @@ import axios from "axios";
 
 @Service()
 export class ScheduleFetchService {
-    async fetchIcsFile(id: string, type: string) {
-        const url = `https://plany.ath.bielsko.pl/plan.php?type=${type}&id=${id}&cvsfile=true`;
+    async fetchIcsFile(id: string, type: string, week: number) {
+        const url = `https://plany.ath.bielsko.pl/plan.php?type=${type}&id=${id}&cvsfile=true&w=${week}`;
 
         const response = await axios.get(url);
         return response.data;
